51b75eb57fad74dbd315bfa701d4ee9fc8388ab0
Shabd Shrivastava <shabdshri64@gmail.com>
2025-11-09 20:22:00 +0000

    further changes

--- demo.c
+++ demo.c
@@ -1,3 +1,5 @@
+#define F_CPU 12000000UL
+
 #include "miniCAN.h"
 #include <avr/io.h>
 #include <util/delay.h>
--- miniCAN.c
+++ miniCAN.c
@@ -1,10 +1,10 @@
+#define F_CPU 12000000UL
+#define BAUD 57600
+
 #include <avr/io.h>
 #include <util/delay.h>
-#include <until/setbaud.h>
-#include "miniCAN.h"
-
-#define F_CPU 12000000UL
-#define BAUD 57600 
+#include <util/setbaud.h>
+#include "miniCAN.h" 
 
 #define START_BYTE 0xAA
 #define MAX_DATA 8
@@ -38,9 +38,10 @@ uint8_t UART_receiveByte(void) {
 // ---------- CRC8 ----------
 uint8_t crc8(const uint8_t *data, uint8_t len) {
     uint8_t crc = 0x00;
-    for (uint8_t i = 0; i < len; i++) {
+    uint8_t i, j;
+    for (i = 0; i < len; i++) {
         crc ^= data[i];
-        for (uint8_t j = 0; j < 8; j++) {
+        for (j = 0; j < 8; j++) {
             if (crc & 0x80) crc = (crc << 1) ^ 0x07;
             else crc <<= 1;
         }
@@ -51,22 +52,24 @@ uint8_t crc8(const uint8_t *data, uint8_t len) {
 // ---------- miniCAN ----------
 
 void miniCAN_sendFrame(MiniCAN_Frame *f) {
+    uint8_t i;
     UART_sendByte(START_BYTE);
     UART_sendByte(f->id);
     UART_sendByte(f->len);
-    for (uint8_t i = 0; i < f->len; i++) UART_sendByte(f->data[i]);
+    for (i = 0; i < f->len; i++) UART_sendByte(f->data[i]);
     UART_sendByte(f->crc);
 }
 
 bool miniCAN_receiveFrame(MiniCAN_Frame *f) {
     uint8_t byte = UART_receiveByte();
+    uint8_t i;
     if (byte != START_BYTE) return false;
 
     f->id = UART_receiveByte();
     f->len = UART_receiveByte();
     if (f->len > MAX_DATA) return false;
 
-    for (uint8_t i = 0; i < f->len; i++)
+    for (i = 0; i < f->len; i++)
         f->data[i] = UART_receiveByte();
 
     f->crc = UART_receiveByte();
